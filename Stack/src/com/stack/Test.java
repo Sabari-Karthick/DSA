de896c5d6e4e84c52b20f2bf3e16d33482cd2f8c
SK <sabariks2003@gmail.com>
2024-07-07 22:40:54 +0530

    Update Stack

--- Stack/src/com/stack/Test.java
+++ Stack/src/com/stack/Test.java
@@ -3,6 +3,8 @@ package com.stack;
 public class Test {
 	public static void main(String[] args) throws Exception {
 		CustomStack stack = new CustomStack(20);
+//		Stack<Integer> stac = new Stack<>();
+//		Collections.min(stac);
 		stack.push("Hi");
 		stack.push("Hello");
 		stack.push("Hoooo");
