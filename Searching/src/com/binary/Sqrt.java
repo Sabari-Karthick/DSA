e75fd95fb4b664c84738c7282376910ec12b28ad
SK <sabariks2003@gmail.com>
2024-07-17 21:40:12 +0530

    Sqrt Problem Added

---
+++ Searching/src/com/binary/Sqrt.java
@@ -0,0 +1,23 @@
+package com.binary;
+
+public class Sqrt {
+
+}
+
+//https://leetcode.com/problems/sqrtx/description/
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        int start = 1;
+        int end = x/2;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if((long)mid * mid > x){
+                end = mid-1;
+            }else {
+                start = mid +1;
+            }
+        }
+        return end;
+    }
+}
