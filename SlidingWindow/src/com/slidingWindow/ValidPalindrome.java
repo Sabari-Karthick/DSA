6927fa8dd41c34a3109a6dba2014f2c8ede7d8ab
SK <sabariks2003@gmail.com>
2024-08-26 22:57:03 +0530

    Sliding Window Added...

---
+++ SlidingWindow/src/com/slidingWindow/ValidPalindrome.java
@@ -0,0 +1,34 @@
+package com.slidingWindow;
+//https://leetcode.com/problems/valid-palindrome/description/
+
+//This problem does not purposefully require Sliding Window but it is better optimized with it.
+public class ValidPalindrome {
+
+}
+
+class Sol124{
+	class Solution {
+	    public boolean isPalindrome(String s) {
+	        int start = 0;
+	        int end = s.length() -1 ;
+	        while(start <= end){
+	            Character i = s.charAt(start);
+	            Character j = s.charAt(end);
+	            if(!Character.isLetterOrDigit(i)){
+	                 start++;
+	                 continue;
+	            }
+	            if(!Character.isLetterOrDigit(j)){
+	                 end--;
+	                 continue;
+	            }
+	            if(Character.toLowerCase(i) != Character.toLowerCase(j)){
+	                return false;
+	            }
+	            start++;
+	            end--;
+	        }
+	        return true;
+	    }
+	}
+}
