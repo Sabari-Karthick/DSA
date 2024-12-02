2d33454c5b97d662d504bc67f0dbd51c36ea53ad
SK <sabariks2003@gmail.com>
2024-12-02 21:19:28 +0530

    CheckIfStringOccursAsPrefix Added

---
+++ Strings/src/com/string/manipulation/CheckIfStringOccursAsPrefix.java
@@ -0,0 +1,21 @@
+package com.string.manipulation;
+
+/**
+ * https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence
+ * 
+ * 
+ * 
+ * 
+ */
+public class CheckIfStringOccursAsPrefix {
+
+}
+class Solution154jn8sfh {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for(int i = 0;i<words.length ; i++){
+            if(words[i].startsWith(searchWord)) return i+1;
+        }
+        return -1;
+    }
+}
