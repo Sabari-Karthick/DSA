144717b0a717002be412e038dffc77ea8b74d1a8
SK <sabariks2003@gmail.com>
2025-02-04 22:06:58 +0530

    ProductExceptSelf Revised

--- Arrays/src/com/array/medium/ProductExceptSelf.java
+++ Arrays/src/com/array/medium/ProductExceptSelf.java
@@ -3,6 +3,17 @@ package com.array.medium;
 import java.util.Arrays;
 //
 //https://leetcode.com/problems/product-of-array-except-self/description/
+/**
+ * The solution lies in the question itself
+ * 
+ * answer[i]=(product of all elements to the left of i)×(product of all elements to the right of i)
+ * 
+ * Key Pattern to Look For:  
+ *     "All elements except the current one" → This hints that the result for each element depends on multiple other parts of the array
+ *      If you can’t modify the array in-place easily, precomputing something (like prefix or suffix products) becomes a strong candidate.
+ * 
+ * 
+ */
 public class ProductExceptSelf {
 	public static void main(String[] args) {
        int[] productExceptSelf = productExceptSelf(new int[]{1,2,3,4});
@@ -22,4 +33,32 @@ public class ProductExceptSelf {
 		}
 		return ans;
 	}
+	
+}
+
+//Reverse Calculation
+class Solution234234ghb {
+    public int[] productExceptSelf(int[] nums) {
+    	int[] ans = new int[nums.length];
+    	int l = 1, r = 1; // They hold the respective side products till i.
+
+    	// Calculate Suffix Product (right products)
+    	for (int i = nums.length - 1; i >= 0; i--) {
+    	    // Initially, ans[i] stores the product of all elements to the right of index i.
+    	    ans[i] = r; 
+    	    // Update r to include nums[i], so the next iteration has the correct right product.
+    	    r *= nums[i];
+    	}
+    	// Calculate Prefix Product (left products) and combine with right products
+    	for (int i = 0; i < nums.length; i++) {
+    	    // Multiply the existing right product (stored in ans[i]) with the left product (l).
+    	    ans[i] *= l; 
+    	    // Update l to include nums[i] for the next iteration.
+    	    l *= nums[i];
+    	}
+
+    	return ans;
+
+
+    }
 }
