0849057b84644962bd69f51295abfa7ad6bcf005
SK <sabariks2003@gmail.com>
2025-02-13 00:08:48 +0530

    LC:70 Climb Stairs Added

---
+++ DynamicProgramming/src/com/batman/Notes.java
@@ -0,0 +1,36 @@
+package com.batman;
+/**
+ * 
+ * Dynamic Programming : 
+ * 
+ * For 1 Dp problems :: 
+ *   Steps : 
+ *         --> Understand Whether the problem needs DP. 
+ *                    --> Patterns : 
+ *                             1.Count Total number of ways (Distinct) , 
+ *                             2.Problems where the result can be arrived in multiple ways but the question ask may be max or min results   
+ * 
+ * 
+ * Whenever Try all possible ways comes in (count,best in the count)  --> it is where recursion comes in.
+ * So first Deside whether the problem is a recursive problem to be solved
+ * 
+ * A way to do Recursion in one of the simple way : 
+ *        1. Try to represent the problem in terms of index (for not an array problem also and for array aswell)
+ *        2. Do all possible stuffs(operations) in that index as per or as said in the problem
+ *        3. if sum -> sum of all stuffs 
+ *           if min -> min of all stuffs
+ *           if max -> max of all stuffs
+ *           
+ *           x = f(y) where x is the result  f() is the required action while y is the action on all the possible ways of that action
+ * 
+ * With these we can write recursion then we can memorize to convert to DP then we can tabulate the redundant results to use later then we can space optimise.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+public class Notes {
+
+}
---
+++ DynamicProgramming/src/com/batman/dp1/ClimbingStairs.java
@@ -0,0 +1,69 @@
+package com.batman.dp1;
+/**
+ * 
+ * 
+ * LC : 70 --> https://leetcode.com/problems/climbing-stairs/description/
+ * 
+ * Question asks Distinct Ways so it is a recursive Problem
+ * 
+ * 
+ * So the recursion is if f(n) is the recursion 
+ * 
+ * f(n) --> number of ways to reach the the number n from 0 (0 to n)
+ * 
+ * The stuff here is jump 1 or jump 2
+ * 
+ * So we can say each step is each index from 0 to n .
+ * 
+ * For our base case to reach 0th step number of ways is 1 
+ * To get to step 1 number of ways is 1 
+ * to step 2 number of ways is 2
+ * 
+ * Then at each index you can do jump 1 or jump 2 which means 
+ * 
+ * FROM (n-1) you jumbed one or from (n-2) you jumped 2 steps so calculate that number of ways for (n-1) and (n-2) steps
+ * 
+ */
+public class ClimbingStairs {
+
+}
+
+
+//Brute Force Recursion
+class Solution {
+    public int climbStairs(int n) {
+     if (n <= 2) return n;
+     return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
+
+
+//Dp 
+class Solution3242 {
+    public int climbStairs(int n) {
+        if (n <= 2) return n; // 0 -> 1 step , 1 -> 1 step, 2 ->2step
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2; // dp[0] = 0 by default
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
+
+//Since we need only two values to be remembered for each step we dont need to store fully
+class Solution234234sfd {
+    public int climbStairs(int n) {
+        if (n <= 2) return n; // 0 -> 1 step , 1 -> 1 step, 2 ->2step
+        int prev2 = 1 , prev1 = 2;
+        for (int i = 3; i <= n; i++) {
+            int cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1=cur;
+        }
+        return prev1;
+    }
+}
+
+
