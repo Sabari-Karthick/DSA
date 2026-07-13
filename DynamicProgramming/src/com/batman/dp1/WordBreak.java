824ae9f32501d80136bb6d5e9a7140990d697c23
SK <sabariks2003@gmail.com>
2026-07-14 00:05:55 +0530

    WordBreak Added

---
+++ DynamicProgramming/src/com/batman/dp1/WordBreak.java
@@ -0,0 +1,52 @@
+package com.batman.dp1;
+
+import java.util.*;
+
+public class WordBreak {
+
+    private static final Map<String, Boolean> memo  = new HashMap<>(); // It stores the recursive results not whether the word exists in dic or not
+    public static void main(String[] args) {
+
+        System.out.println(wordBreak("leetcode", List.of("leet", "code"))); // true
+        System.out.println(wordBreak("applepenapple", List.of("apple", "pen"))); // true
+        System.out.println(wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat"))); // false
+    }
+
+    // What's the Brute Force ??
+    // We can cut the string in each of its gap so it is 2 ^ (N - 1) ways and then with those chunks and we can run the loop
+    // This is exponential time complexity
+
+    // Other way instead of cutting the string in each gap we can check if the string is present in the dictionary or not and then we can check for the remaining string and so on
+    // That is DFS
+
+    // But if you see with different combination we may get into check the same word again and again is in dictionary if we use plain loop
+    // With Plain DFS we get Time Exceed . So We need memorization
+    public static boolean wordBreak(String s, List<String> wordDict) {
+
+        if(memo.containsKey(s)) return memo.get(s);
+
+        if(wordDict.contains(s)){ // Avoid this by having a empty check plus running loop till the length
+            memo.put(s, true);
+            return true;
+        }
+
+        // Start at index 1 the first word
+        for (int i = 1; i < s.length(); i++) {
+            String left = s.substring(0, i);
+            String right = s.substring(i);
+
+            // Check if the left word is in the dictionary
+            if (wordDict.contains(left)) {
+                // We need to now know whether the remaining part is valid
+                if (wordBreak(right, wordDict)) {
+                    memo.put(s, true); // Remember that the
+                    return true;
+                } // Else Continue the loop
+            }
+
+        }
+
+        memo.put(s, false);
+        return false;
+    }
+}
